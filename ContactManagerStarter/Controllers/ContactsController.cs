704bbc457ca00b78530bc373c5dc19ea3e04722f
Pranathi <Pranathi@PranathiVM>
2023-06-10 17:58:31 +0000

    add primary email

--- ContactManagerStarter/Controllers/ContactsController.cs
+++ ContactManagerStarter/Controllers/ContactsController.cs
@@ -76,15 +76,18 @@ namespace ContactManager.Controllers
         {
             var contactList = await _context.Contacts
                 .OrderBy(x => x.FirstName)
+                .Include(x => x.EmailAddresses)
                 .ToListAsync();
 
-            return PartialView("_ContactTable", new ContactViewModel { Contacts = contactList });
+            var contactsVM = contactList.Select(a => new ContactViewModel(a)).ToList();
+
+            return PartialView("_ContactTable", contactsVM);
         }
 
         public IActionResult Index()
-            {
-                return View();
-            }
+        {
+            return View();
+        }
 
         public IActionResult NewContact()
         {
@@ -92,7 +95,7 @@ namespace ContactManager.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> SaveContact([FromBody]SaveContactViewModel model)
+        public async Task<IActionResult> SaveContact([FromBody] SaveContactViewModel model)
         {
             var contact = model.ContactId == Guid.Empty
                 ? new Contact { Title = model.Title, FirstName = model.FirstName, LastName = model.LastName, DOB = model.DOB }
@@ -163,7 +166,7 @@ namespace ContactManager.Controllers
 
             message.Body = new TextPart("plain")
             {
-                Text = "Contact with id:" + contactId.ToString() +" was updated"
+                Text = "Contact with id:" + contactId.ToString() + " was updated"
             };
 
             using (var client = new SmtpClient())
--- ContactManagerStarter/Models/ContactViewModel.cs
+++ ContactManagerStarter/Models/ContactViewModel.cs
@@ -8,6 +8,21 @@ namespace ContactManager.Models
 {
     public class ContactViewModel
     {
-        public List<Contact> Contacts { get; set; }
+        private Contact a;
+
+        public ContactViewModel(Contact contact)
+        {
+            this.Id = contact.Id;
+            this.Title = contact.Title;
+            this.FirstName = contact.FirstName;
+            this.LastName = contact.LastName;
+            this.PrimaryEmailAddress = contact.EmailAddresses.Any(a => a.Type == EmailType.Primary) ? contact.EmailAddresses.First(a => a.Type == EmailType.Primary).Email : contact.EmailAddresses.First().Email;
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PrimaryEmailAddress { get; set; }
     }
 }
